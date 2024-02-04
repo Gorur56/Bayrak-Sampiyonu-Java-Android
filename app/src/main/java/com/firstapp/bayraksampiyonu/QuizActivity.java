ebabdeb4e9223275ecbc6fc55715ec974c33c9af
Gorur56 <sevim.gorur01@gmail.com>
2024-02-04 22:28:18 +0300

    Merge

---
+++ app/src/main/java/com/firstapp/bayraksampiyonu/QuizActivity.java
@@ -0,0 +1,39 @@
+package com.firstapp.bayraksampiyonu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private TextView textViewDogru, textViewYanlis, textViewSoruSayi;
+    private ImageView imageViewBayrak;
+    private Button buttonA, buttonB, buttonC, buttonD;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        textViewDogru = findViewById(R.id.textViewDogru);
+        textViewYanlis = findViewById(R.id.textViewYanlis);
+        textViewSoruSayi = findViewById(R.id.textViewSoruSayi);
+
+        imageViewBayrak = findViewById(R.id.imageViewBayrak);
+
+        buttonA = findViewById(R.id.buttonA);
+        buttonB = findViewById(R.id.buttonB);
+        buttonC = findViewById(R.id.buttonC);
+        buttonD = findViewById(R.id.buttonD);
+
+        buttonA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+    }
+}
