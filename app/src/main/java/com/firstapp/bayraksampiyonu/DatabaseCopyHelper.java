5574a9998ab605bde8da6f5a690d7459fb5946ea
Gorur56 <sevim.gorur01@gmail.com>
2024-02-05 21:50:13 +0300

    Merge

--- app/src/main/java/com/firstapp/bayraksampiyonu/DatabaseCopyHelper.java
+++ app/src/main/java/com/firstapp/bayraksampiyonu/DatabaseCopyHelper.java
@@ -1,4 +1,4 @@
-package com.info.bayrakquiz;
+package com.firstapp.bayraksampiyonu;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
--- app/src/main/java/com/firstapp/bayraksampiyonu/MainActivity.java
+++ app/src/main/java/com/firstapp/bayraksampiyonu/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonBasla;
@@ -17,11 +19,23 @@ public class MainActivity extends AppCompatActivity {
 
         buttonBasla = findViewById(R.id.buttonBasla);
 
+        veritabaniKopyala(); // İlk açıldığında bir kere veritabanını kopyalayacak.
         buttonBasla.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,QuizActivity.class));
             }
         });
+    }
+
+    public void veritabaniKopyala() throws IOException {
+        DatabaseCopyHelper helper = new DatabaseCopyHelper(this);
+        try {
+            helper.createDataBase();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        helper.openDataBase();
     }
 }
